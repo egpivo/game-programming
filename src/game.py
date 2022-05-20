d1c41f9b901cf1740dc049b99246961697d43422
Wen-Ting Wang <egpivo@gmail.com>
2022-05-20 21:44:22 +0800

    CHG: create a basic GUI layout

---
+++ src/game.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+class Game(tk.Frame):
+    def __init__(self, master: tk.Tk) -> None:
+        super().__init__(master)
+        self.lives = 3
+        self.width = 600
+        self.height = 400
+        self.canvas = tk.Canvas(
+            self,
+            bg="#405DE6",
+            width=self.width,
+            height=self.height
+        )
+        self.canvas.pack()
+        self.pack()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Hello, Pong!")
+    game = Game(root)
+    game.mainloop()
