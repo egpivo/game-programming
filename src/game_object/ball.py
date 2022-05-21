381deeb15c73302b398cfe874ce0b41e887b895d
Wen-Ting Wang <egpivo@gmail.com>
2022-05-21 22:40:47 +0800

    CHG: reorganize the folder for game objects; add a new object - ball

---
+++ src/game_object/ball.py
@@ -0,0 +1,16 @@
+from .game_object import GameObject
+
+class Ball(GameObject):
+    def __init__(self, canvas, x, y):
+        self.radius = 10
+        self.direction = [1, -1]
+        self.speed = 10
+        item = canvas.create_oval(
+            x - self.radius,
+            y - self.radius,
+            x + self.radius,
+            y + self.radius,
+            fill="white"
+        )
+
+        super().__init__(canvas, item)
